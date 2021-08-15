abf41249b3529a192384d94445b27215d417c0d4
Skazka-netpeak <skazka.netpeak@gmail.com>
2021-08-15 18:55:43 +0300

    Connection with webdriver

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Skazka\\autotests\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://tempura.od.ua");
+
+        driver.close();
+    }
+}
